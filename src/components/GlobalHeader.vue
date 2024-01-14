d1db2906824e214cb58f64d40c5dfb55ff8a2cec
KarlexYan <karlexyan@aliyun.com>
2024-01-14 15:08:25 +0800

    [add] 前端初始化,配置全局模板布局

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="globalHeader">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img
+            class="logo"
+            src="https://pic.imgdb.cn/item/65a34cbe871b83018a23e7f2.png"
+            alt=""
+          />
+        </div>
+      </a-menu-item>
+      <a-menu-item
+        v-for="item in routes"
+        :key="item.path"
+        style="font-size: 20px"
+      >
+        {{ item.name }}
+      </a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+// 默认主页
+const selectedKeys = ref(["/"]);
+
+// 路由跳转后,更新选中菜单项
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
+
+<style setup scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.title-bar .logo {
+  border-radius: 2px;
+  cursor: auto;
+  background: red;
+}
+</style>
---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,47 @@
+<!--全局基本模板-->
+<template>
+  <div id="baseLayout">
+    <a-layout style="height: 400px">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/KarlexYan" target="_blank">
+          YOJ By KalrexYan
+        </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#baseLayout .header {
+  margin-bottom: 16px;
+  background: red;
+}
+
+#baseLayout .content {
+  margin-bottom: 16px;
+  background: linear-gradient(to right, #bbb, #eee);
+}
+
+#baseLayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  text-align: center;
+}
+</style>
+<script>
+import GlobalHeader from "@/components/GlobalHeader";
+
+export default {
+  components: { GlobalHeader },
+};
+</script>
