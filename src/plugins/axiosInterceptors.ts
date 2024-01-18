a7cf120bf9be5c7abb59819f1802e90c49065ffb
KarlexYan <karlexyan@aliyun.com>
2024-01-19 00:35:18 +0800

    [add] 新增axios全局拦截器

---
+++ src/plugins/axiosInterceptors.ts
@@ -0,0 +1,29 @@
+// Add a request interceptor
+import axios from "axios";
+
+axios.interceptors.request.use(
+  function (config) {
+    // Do something before request is sent
+    console.log("请求拦截器");
+    return config;
+  },
+  function (error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function (response) {
+    // Any status code that lie within the range of 2xx cause this function to trigger
+    // Do something with response data
+    console.log("响应拦截器");
+    return response;
+  },
+  function (error) {
+    // Any status codes that falls outside the range of 2xx cause this function to trigger
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
