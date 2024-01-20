fcba7a41a50fd067e3f06ab840ad89ba4eb9bfe7
KarlexYan <karlexyan@aliyun.com>
2024-01-20 22:38:56 +0800

    [style] 统一class名

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="home">
+  <div class="admin">
     仅管理员可见
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
@@ -13,7 +13,7 @@ import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 export default defineComponent({
   name: "AdminView",
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 });
 </script>
