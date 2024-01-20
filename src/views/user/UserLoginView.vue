24914ce6ee3c78bf9d66cccc43bf93ed4768441a
KarlexYan <karlexyan@aliyun.com>
2024-01-21 01:28:03 +0800

    [add] 实现调用用户登录

--- src/views/user/UserLoginView.vue
+++ src/views/user/UserLoginView.vue
@@ -1,3 +1,55 @@
 <template>
-  <div class="userLoginView">登录页</div>
+  <div class="userLoginView">
+    <a-form :model="form" :style="{ width: '600px' }" @submit="handleSubmit">
+      <a-form-item field="userAccount" label="用户名">
+        <a-input v-model="form.userAccount" placeholder="请输入你的账户" />
+      </a-form-item>
+      <a-form-item field="userPassword" label="密码">
+        <a-input-password
+          v-model="form.userPassword"
+          placeholder="密码不少于8位"
+        />
+      </a-form-item>
+      <a-form-item>
+        <a-button html-type="submit">登录</a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
+
+<script setup lang="ts">
+import { reactive } from "vue";
+import { UserControllerService, UserLoginRequest } from "../../../backapi";
+import message from "@arco-design/web-vue/es/message";
+
+/**
+ * 表单信息
+ */
+const form = reactive({
+  userAccount: "",
+  userPassword: "",
+} as UserLoginRequest);
+
+/**
+ * 表单提交
+ * */
+const handleSubmit = async () => {
+  const res = await UserControllerService.userLoginUsingPost(form);
+  if (res.code === 0) {
+    // 登录成功
+    message.success("登录成功" + JSON.stringify(res.data));
+  } else {
+    message.error("登录失败" + res.message);
+  }
+};
+</script>
+<style>
+/*去除IE EDGE的密码默认的快速清除按钮以及密码文字显示按钮*/
+input[type="password"]::-ms-reveal {
+  display: none;
+}
+
+input[type="password"]::-ms-clear {
+  display: none;
+}
+</style>
